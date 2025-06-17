e6a65888aa0fc1f10b592eda65dd28716efe9d44
SUBHASISH NAYAK <subhasishnayak88@gmail.com>
2025-06-17 20:58:03 +0530

    allow local host and self.

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://focal-point-ecru.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
